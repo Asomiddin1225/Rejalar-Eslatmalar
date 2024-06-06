8ec2849d1276bfa19b089c8ae380ea7fb6c34e4f
Asomiddin <154572407+Asomiddin1225@users.noreply.github.com>
2024-06-06 11:19:40 +0500

    first commit

---
+++ lib/lib1/page/flutter_page.dart
@@ -0,0 +1,111 @@
+import 'package:dars_49/lib1/page/darsvideo.dart';
+import 'package:dars_49/lib1/page/darsvieo.dart';
+import 'package:flutter/material.dart';
+
+class FlutterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Flutter'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.grey[200],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      'assets/flutter.png',
+                      width: double.infinity,
+                      height: 130,
+                      fit: BoxFit.cover,
+                    ),
+                    const SizedBox(height: 10),
+                    const Row(
+                      children: [
+                        Text(
+                          'Name: ',
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                        ),
+                        Text(
+                          'Flutter bilan tanshuv',
+                          style: TextStyle(fontSize: 15, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 5),
+                    const Row(
+                      children: [
+                        Text(
+                          'Tafsya: ',
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                        ),
+                        Text(
+                          "Kompiyuterga Flutter o'rnatish\nAndroid Studio o'rnatish...  ",
+                          style: TextStyle(fontSize: 11, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Darsga oid Vidyolar',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FlutterVido()),
+                );
+              },
+              child: const Text('vido 1'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FlutterVido2()),
+                );
+              },
+              child: const Text('vido 2'),
+            ),
+            const Spacer(),
+            const Text(
+              'Narxi: \$199',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
